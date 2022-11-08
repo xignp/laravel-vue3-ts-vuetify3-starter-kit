21cb892eb8235ee4105a2859ce307ca35c9243ee
xignp <xi6108gn@gmail.com>
2022-11-09 01:20:52 +0900

    fix vite directive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,11 +10,12 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-    @vite(['resources/sass/app.scss', 'resources/js/app.ts'])
+    @vite('resources/sass/app.scss')
 </head>
 
 <body>
     <div id="app"></div>
+    @vite('resources/js/app.ts')
 </body>
 
 </html>
